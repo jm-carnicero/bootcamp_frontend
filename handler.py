8c517cbfb751b5dec4b7c0787204210efc0a569d
nelson.nunes@ibm.com <nelson.nunes@ibm.com>
2021-10-28 21:22:03 +0200

    add memes api

--- handler.py
+++ handler.py
@@ -1,5 +1,20 @@
 import tornado.ioloop
 import tornado.web
+import requests
+import json
+
+def get_meme():
+  result = requests.get(url = 'https://meme-api.herokuapp.com/gimme')
+  return result.json()
+
+class MemesHandler(tornado.web.RequestHandler):
+    def get(self):
+        count = self.get_arguments("count")[0]
+        data = []
+        for x in range(int(count)):
+            meme = get_meme()
+            data.append(meme['url'])
+        self.write(json.dumps(data))
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -8,6 +23,7 @@ class MainHandler(tornado.web.RequestHandler):
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
+        (r"/memes", MemesHandler),
     ])
 
 if __name__ == "__main__":
